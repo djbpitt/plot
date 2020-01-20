0e2d253313d407e6dd17d37c405c56c748fa540e
David J Birnbaum <djbpitt@gmail.com>
2020-01-19 20:52:50 -0500

    add thread timings to XML Prague paper

---
+++ nw/threads/test_threads.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+for j in {1..15}
+do
+    for i in {1..10}
+        do
+          echo "$j paragraph(s) with $i thread(s)"
+          java -Xms24G -Xmx24G -jar /Users/djb/bin/saxon_ee/saxon9ee.jar -sa -it -o:/dev/null -repeat:10 nw_ee_threads.xsl paragraph_count=$j output_grid=false threads=$i
+          # java -Xms24G -Xmx24G -jar /usr/local/Cellar/saxon/9.9.1.4/libexec/saxon9he.jar -it -o:/dev/null -repeat:10 nw_he.xsl paragraph_count=$i output_grid=false
+        done
+done
+
